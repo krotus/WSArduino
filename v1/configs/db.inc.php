e5a9b9902d9c35a46c5b0a61b404bfb752ef32a6
Andreu Sala <andreus.sala@gmail.com>
2016-04-13 18:04:39 +0200

    ignore files .inc.php

--- v1/configs/db.inc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// Constants per conectar-se a la base de dades
-define("HOST_NAME", "localhost");
-define("DATABASE_NAME", "arduino");
-define("USER", "root");
-define("PASSWORD", "password");
-
-?>
